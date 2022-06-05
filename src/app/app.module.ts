54abea97560ec0b8fb9cf6dc8316f4f27af1d605
tornike.gegeshidze <tokagegesha@gmail.com>
2022-06-05 12:28:14 +0400

    nev ver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SafePipe } from './safe.pipe';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,SafePipe
   ],
   imports: [
     BrowserModule
