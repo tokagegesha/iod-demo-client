bf3e2b4f7719d8ab128da7ef6a3a10ab8202236e
tornike.gegeshidze <tokagegesha@gmail.com>
2022-06-06 11:53:33 +0400

    changed ifrmale url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Form, NgForm} from "@angular/forms";
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,18 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private domSanitizer: DomSanitizer) {
+  }
 
   yt = '<iframe class="iod-frame"  allow="autoplay; encrypted-media" allowfullscreen></iframe>';
   title = 'iod-demo-client';
+
+  iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(`https://exat.exa.loanhdsandbox.com/source/ui`);
+
+  onSubmit = (f: NgForm) => {
+    /*   if(f.token){
+         this.iframeUrl+=
+       }*/
+    console.log(f.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SafePipe } from './safe.pipe';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,SafePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
